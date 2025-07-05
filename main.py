517fff0deeb1f0f4a3b7fef2e872eeac78041531
liv553 <lba3@cesar.school>
2025-07-05 14:12:27 -0300

    game base created

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    number = 1
+    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    while number !=0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            screen.fill("black")
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
