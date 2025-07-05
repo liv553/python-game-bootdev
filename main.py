f33fccff55320c5ad4f50d29c4343febfe10db1c
liv553 <lba3@cesar.school>
2025-07-05 18:43:27 -0300

    game asteroids, player and logic add

--- main.py
+++ main.py
@@ -1,19 +1,65 @@
-import pygame
+# main.py
 
+import pygame
 from constants import *
+from player import Player
+from asteroidfield import *
+from asteroid import *
+from circleshape import *
 
 def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    number = 1
-    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-    while number !=0:
+    pygame.display.set_caption("Asteroids")
+
+    clock = pygame.time.Clock()
+    
+    dt = 0
+
+    asteroids = pygame.sprite.Group()
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = updatables, drawables
+    Asteroid.containers = asteroids, updatables, drawables
+    AsteroidField.containers = (updatables,)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroid_field = AsteroidField()
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            screen.fill("black")
-            pygame.display.flip()
+                running = False
 
+        updatables.update(dt)
+
+        for asteroid in asteroids:
+            if player.is_there_collision(asteroid):
+                print("Game over!")
+                running = False
+                break
+        
+        if not running:
+            break
+
+        screen.fill("black")
+
+        for sprite in drawables:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
